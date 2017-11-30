6260249462270a94de9d774b7786fa36ba6e04b7
Francis Levasseur <f.levasseur@technosolutionscl.com>
2017-11-30 14:36:15 -0500

    cron on off

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+if [ "$ENABLE_CRON" = "TRUE" ];then
+	#Generate a random time for syncing
+	echo "$(($RANDOM%60)) */2 * * * /mirror_ubuntu.sh" > /cron
 
-#Generate a random time for syncing
-echo "$(($RANDOM%60)) */2 * * * /mirror_ubuntu.sh" > /cron
+	#add it
+	crontab /cron
 
-#add it
-crontab /cron
-
-#start services
-service cron start
+	#start services
+	service cron start
+fi
 service nginx start
 
 #live forever
