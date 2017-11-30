3b6b0653a752bbb6dcce07f69ac59e379a9e33ae
Francis Levasseur <f.levasseur@technosolutionscl.com>
2017-11-30 10:25:17 -0500

    mirror log

--- mirror_ubuntu.sh
+++ mirror_ubuntu.sh
@@ -4,6 +4,6 @@ cd /ubuntu_mirror
 
 #RSYNC=/usr/bin/rsync
 
-rsync -rtlH --delete-after --delay-updates --safe-links --max-delete=3000 rsync://ubuntu.mirror.iweb.ca/ubuntu/ ubuntu/ > /dev/null
+rsync -rtlH --delete-after --delay-updates --safe-links --max-delete=3000 rsync://ubuntu.mirror.iweb.ca/ubuntu/ ubuntu/ > _last_sync.log
 
 date > __last_sync
